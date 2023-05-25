191b75c5d6ba7d269deae39baf851144ed36fcad
SangDal <sangdal91@naver.com>
2023-05-25 22:36:16 +0900

    두번쨰 시도

--- config.js
+++ config.js
@@ -29,6 +29,7 @@ export const config = {
         database: required('DB_DATABASE'),
         password: required('DB_PASSWORD')
     },
+    port:parseInt(required('PORT', 8080)),
     cors: {
         allowedOrigin: required('CORS_ALLOW_ORIGIN')
     }
--- db/database.js
+++ db/database.js
@@ -1,4 +1,3 @@
-import mysql from 'mysql2';
 import { config } from '../config.js';
 import mariadb from 'mariadb';
 
