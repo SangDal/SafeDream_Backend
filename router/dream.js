43558c33cf288c1e49caa5f911705fd2c73ca4e9
SangDal <131240971+SangDal@users.noreply.github.com>
2023-05-26 14:38:57 +0900

    Update dream.js

--- router/dream.js
+++ router/dream.js
@@ -9,6 +9,7 @@ router.post('/register', userController.register); // 회원 가입
 router.get('/check', userController.checkUserId); // 아이디 중복 체크
 router.put('/update', isAuth, userController.updateUser); // 회원 정보 수정
 router.delete('/:userid', isAuth, userController.deleteUser); // 회원 삭제
+router.post('/findUserId', findUserId); // 아이디 찾기
 
 // 보호자 전화번호 받기 
 router.get('/guardianHp', isAuth, userController.guardianHp);
