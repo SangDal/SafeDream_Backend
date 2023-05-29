fe2e53ee03f8bb400a2e3bf41f9aad8e42d4cb67
SangDal <131240971+SangDal@users.noreply.github.com>
2023-05-29 17:43:06 +0900

    Update user.js
    계정수정 부분 수정

--- router/user.js
+++ router/user.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/', isAuth, userController.getAll);// 정보 조회
 router.get('/:userid', isAuth, userController.getOne);// 정보 조회
 router.post('/',isAuth, userController.createUser);// 계정 추가
-router.put('/:userid',isAuth, userController.updateUser);// 계정 수정
+router.put('/',isAuth, userController.updateUser);// 계정 수정
 router.delete('/:userid',isAuth, userController.deleteUser);// 계정 삭제 
 
 export default router;
